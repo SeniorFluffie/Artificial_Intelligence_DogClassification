d8a7fd51268cc70606c44e29028cd1c5c9a385d5
SeniorFluffie <michael.kazman@gmail.com>
2021-02-28 05:19:39 -0500

    added main function (and expected outputs)

--- assignment2.py
+++ assignment2.py
@@ -22,3 +22,13 @@ def fuzzy_classifier(input):
   # highest_membership_class is a string indicating the highest membership class, either "beagle", "corgi", "husky", or "poodle"
   # class_memberships is a four element list indicating the membership in each class in the order [beagle probability, corgi probability, husky probability, poodle probability]
   return highest_membership_class, class_memberships
+
+if __name__ == "__main__":
+  print('------------------------------------------------------Example 1------------------------------------------------------')
+  most_likely_class, class_probabilities = naive_bayes_classifier([59, 32, 17])
+  print('Actual Output:   ', most_likely_class, ',', class_probabilities)
+  print('Expected output: ', '"corgi", [0.0017212721198989387, 0.8444998269543612, 0.003430911965564154, 0.15034798896017576]')
+  print('------------------------------------------------------Example 2------------------------------------------------------')
+  most_likely_class, class_probabilities = naive_bayes_classifier([65, 55, 30])
+  print('Actual Output:   ', most_likely_class, ',', class_probabilities)
+  print('Expected output: ', '"poodle",  [2.4342947465727013e-29, 7.511501676401435e-37, 0.2390117475045523, 0.7609882524954477]')
