b2800ac08d86adebeb7383579749f8daced4608c
SeniorFluffie <michael.kazman@gmail.com>
2021-03-04 00:16:59 -0500

    implemented fuzzy classifier and test cases

--- assignment2.py
+++ assignment2.py
@@ -31,15 +31,20 @@ def fuzzy_classifier(input):
   return highest_membership_class, class_memberships
 
 if __name__ == "__main__":
-  print('------------------------------------------------------Example 1------------------------------------------------------')
+  print('------------------------------------------------------Example 1: Naive------------------------------------------------------')
   most_likely_class, class_probabilities = naive_bayes_classifier([59, 32, 17])
   print('Actual Output:   ', most_likely_class, ',', class_probabilities)
   print('Expected output: ', '"corgi", [0.0017212721198989387, 0.8444998269543612, 0.003430911965564154, 0.15034798896017576]')
-  print('------------------------------------------------------Example 2------------------------------------------------------')
+  print('------------------------------------------------------Example 2: Naive------------------------------------------------------')
   most_likely_class, class_probabilities = naive_bayes_classifier([65, 55, 30])
   print('Actual Output:   ', most_likely_class, ',', class_probabilities)
   print('Expected output: ', '"poodle",  [2.4342947465727013e-29, 7.511501676401435e-37, 0.2390117475045523, 0.7609882524954477]')
+  print('------------------------------------------------------Example 1: Fuzzy------------------------------------------------------')
   highest_membership_class, class_memberships = fuzzy_classifier([59, 32, 17])
   print('Actual Output:   ', highest_membership_class, ',', class_memberships)
   print('Expected output: ', '"corgi", [0.0, 0.5333333333333333, 0, 0.0]')
+  print('------------------------------------------------------Example 2: Fuzzy------------------------------------------------------')
+  highest_membership_class, class_memberships = fuzzy_classifier([65, 55, 30])
+  print('Actual Output:   ', highest_membership_class, ',', class_memberships)
+  print('Expected output: ', '"poodle", [0.0, 0.0, 0.125, 0.375]')
   
--- fuzzyclassifier.py
+++ fuzzyclassifier.py
@@ -1,9 +1,9 @@
 class FuzzyClassifier(object):
     # dictionary containing the probabilities of each class as well as the mean and deviation of their defining characteristics
     __class_characteristics = {
-        'girth': [[0, 0, 40, 50], [40, 50, 60, 70], [60, 70, 100, 100]],
-        'height': [[0, 0, 25, 40], [25, 40, 50, 60], [50, 60, 100, 100]],
-        'weight': [[0, 0, 5, 15], [5, 15, 20, 40], [20, 40, 100, 100]],
+        'girth': {'small': (0, 0, 40, 50), 'medium': (40, 50, 60, 70), 'large': (60, 70, 100, 100)},
+        'height': {'short': (0, 0, 25, 40), 'medium': (25, 40, 50, 60), 'tall': (50, 60, 100, 100)},
+        'weight': {'light': (0, 0, 5, 15), 'medium': (5, 15, 20, 40), 'heavy': (20, 40, 100, 100)},
     }
 
     def __init__(self, input):
@@ -11,18 +11,52 @@ class FuzzyClassifier(object):
         girth, height, weight = input
 
         # execute naive bayes
-        self.__highest_membership_class, self.__class_memberships = self.classify(girth, height, weight)
+        self.__highest_membership_class, self.__class_memberships = self.__classify(girth, height, weight)
 
     # calculate most likely class
-    def classify(self, girth, height, width):
-        # TODO
-        return (None, None)
+    def __classify(self, girth, height, weight):
+        memberships = self.__get_membership((girth, height, weight))
+        # get the strengths of all fuzzy rules
+        rule_strengths = self.__get_rule_strengths(memberships)
+        # retrieve most likely class
+        most_likely_class = max(rule_strengths, key=lambda x:x[1])[0]
+        # format class memberships (to only show values)
+        class_memberships = [value for breed, value in rule_strengths]
+        return (most_likely_class, class_memberships)
+
+    def __get_membership(self, inputs):
+        # store memberships
+        memberships = {}
+        # go through each of the characteristics (girth, height, etc.)
+        for index, characteristic_name in enumerate(self.__class_characteristics):
+            # get the value of the characteristic (and corresponding input value)
+            characteristic = self.__class_characteristics[characteristic_name]
+            x = inputs[index]
+            # calculate membership for each size
+            sizes = {size: self.__calculate_membership(x, characteristic[size]) for size in characteristic}
+            # set value in membership dict
+            memberships[characteristic_name] = sizes
+        return memberships
+        # go through and calculate value for each characteristic (and size)
+        
+
+    def __get_rule_strengths(self, memberships):
+        # list of all strengths
+        rule_strengths = []
+        # get individual elements (for convenience)
+        girth, height, weight = memberships['girth'], memberships['height'], memberships['weight']
+        # add fuzzy rules
+        rule_strengths.append(('"beagle"', self.__fuzzy_and(height['medium'], self.__fuzzy_or(girth['small'], weight['light']))))
+        rule_strengths.append(('"corgi"', self.__fuzzy_and(self.__fuzzy_and(girth['medium'], height['short']), weight['medium'])))
+        rule_strengths.append(('"husky"', self.__fuzzy_and(self.__fuzzy_and(girth['large'], height['tall']), weight['medium'])))
+        rule_strengths.append(('"poodle"', self.__fuzzy_and(self.__fuzzy_or(girth['medium'], height['medium']), weight['heavy'])))
+        # return list of fuzzy values
+        return rule_strengths
 
     # function for calculating fuzzy membership
-    def fuzzy_membership(self, x, values):
+    def __calculate_membership(self, x, values):
         # values of the trapezoid shape
         a, b, c, d = values
-
         # trapedoizal function describing each characteristic
         if (x <= a):
             return 0
@@ -34,10 +68,17 @@ class FuzzyClassifier(object):
             return (d - x) / (d - c)
         elif (d <= x):
             return 0
+        
+    # goguen t-norm (AND)
+    def __fuzzy_and(self, x, y):
+        return x * y
+
+    # goguen s-norm (OR)
+    def __fuzzy_or(self, x, y):
+        return (x + y) - (x * y)
 
     # return most likely class
     def get_highest_membership_class(self):
-        # TODO
         return self.__highest_membership_class
     
     # return list of all membership probabilities
